2d8aad57713a0eef5b19e163e90f7b8d30e3ccb9
tylerben <ben.tyler@lrewater.com>
2018-08-27 06:57:53 -0600

    Edit screenshots and re-configure image_urls property for each project.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
+// Declare dependencies
 const express = require('express');
 const router = express.Router();
 const data = require('../data.json');
 const { projects } = data;
 
+// Home route
 router.get('/', (req, res) => {
   res.render('index', { projects });
 });
 
+// About route
 router.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Project route
+// if user tries to access route without id specified, redirect to home page
 router.get('/project', (req, res) => {
-  res.render('project');
+  res.redirect('/');
 })
 
+// Project route
 router.get('/project/:id', (req, res) => {
   const { id } = req.params;
   const project = projects[id];
   res.render('project', { project });
 })
 
+// Export routes
 module.exports = router;
